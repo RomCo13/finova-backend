04909fbd0a36b363ac96e5eb4d1b7166494726c1
RomCo13 <romcohen115@gmail.com>
2025-01-03 11:00:30 +0200

    add nodemon

--- finova-backend/src/stocks/stocks.service.ts
+++ finova-backend/src/stocks/stocks.service.ts
@@ -4,8 +4,8 @@ import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class StocksService {
-  private finnhubBaseUrl = 'https://finnhub.io/api/v1';
-  private alphaVantageBaseUrl = 'https://www.alphavantage.co/query';
+  private liveStocksBaseUrl = this.configService.get<string>('LIVE_STOCKS_BASE_URL');
+  private historeyStocksBaseUrl = this.configService.get<string>('HISTORY_STOCKS_BASE_URL');
 
   constructor(
     private readonly httpService: HttpService,
@@ -14,14 +14,14 @@ export class StocksService {
 
   async getStockProfile(symbol: string): Promise<any> {
     const apiKey = this.configService.get<string>('FINNHUB_API_KEY');
-    const url = `${this.finnhubBaseUrl}/stock/profile2?symbol=${symbol}&token=${apiKey}`;
+    const url = `${this.liveStocksBaseUrl}/stock/profile2?symbol=${symbol}&token=${apiKey}`;
     const response = await this.httpService.axiosRef.get(url);
     return response.data;
   }
 
   async getAlphaVantageData(symbol: string): Promise<any> {
     const apiKey = this.configService.get<string>('ALPHA_VANTAGE_API_KEY');
-    const url = `${this.alphaVantageBaseUrl}?function=TIME_SERIES_DAILY&symbol=${symbol}&apikey=${apiKey}&outputsize=compact`;
+    const url = `${this.historeyStocksBaseUrl}?function=TIME_SERIES_DAILY&symbol=${symbol}&apikey=${apiKey}&outputsize=compact`;
     const response = await this.httpService.axiosRef.get(url);
     return response.data;
   }
