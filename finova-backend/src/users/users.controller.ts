137b91bc0ec66f62c1c3306fa1f8ea6cfa24d7a4
Bar Achdut <bar.achdut@justeattakeaway.com>
2025-01-03 11:40:51 +0200

    Update controller

--- finova-backend/src/users/users.controller.ts
+++ finova-backend/src/users/users.controller.ts
@@ -1,23 +1,17 @@
-import { Controller, Get, Query } from '@nestjs/common';
-import { ApiTags, ApiOperation, ApiQuery } from '@nestjs/swagger';
+import { Controller, Post, Body } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiBody } from '@nestjs/swagger';
 import { UsersService } from './users.service';
+import { signUpUserDto } from './dto/sign-up-user.dto'; 
 
 @ApiTags('users')
 @Controller('users')
 export class UsersController {
-  constructor(private readonly UsersService: UsersService) {}
+  constructor(private readonly usersService: UsersService) {}
 
-  @Get('profile')
-  @ApiOperation({ summary: 'Get stock profile from Finnhub' })
-  @ApiQuery({ name: 'symbol', description: 'Stock symbol', required: true })
-  async getStockProfile(@Query('symbol') symbol: string) {
-    return this.stocksService.getStockProfile(symbol);
-  }
-
-  @Get('alpha-vantage')
-  @ApiOperation({ summary: 'Get stock data from Alpha Vantage' })
-  @ApiQuery({ name: 'symbol', description: 'Stock symbol', required: true })
-  async getAlphaVantageData(@Query('symbol') symbol: string) {
-    return this.stocksService.getAlphaVantageData(symbol);
+  @Post('SignUpUser')
+  @ApiOperation({ summary: 'Register a new user' })
+  @ApiBody({ type: signUpUserDto, description: 'The required data for registering a new user' })
+  async signUpUser(@Body() signUpUserRequest: signUpUserDto) {
+    return this.usersService.signUpUser(signUpUserRequest);
   }
 }
