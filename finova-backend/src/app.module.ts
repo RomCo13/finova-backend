181ba350e0487abdafb5e99738538806be1a538b
Bar Achdut <bar.achdut@justeattakeaway.com>
2025-01-03 12:29:37 +0200

    Module

--- finova-backend/src/app.module.ts
+++ finova-backend/src/app.module.ts
@@ -4,11 +4,18 @@ import { AppService } from './app.service';
 import { StocksModule } from './stocks/stocks.module';
 import { StocksController } from './stocks/stocks.controller';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [ ConfigModule.forRoot({ isGlobal: true }),
-    StocksModule],
+    StocksModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/BarAchdut'),
+    UsersModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+
