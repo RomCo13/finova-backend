d3aa3e23df9d65ff4e305c86346219acd7f9c68a
Bar Achdut <bar.achdut@justeattakeaway.com>
2025-01-03 13:15:01 +0200

    Validations

--- finova-backend/src/users/dto/sign-up-user.dto.ts
+++ finova-backend/src/users/dto/sign-up-user.dto.ts
@@ -1,12 +1,20 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsNotEmpty, IsEmail, MinLength } from 'class-validator';
 
-export class signUpUserDto {
+export class SignUpUserDto {
   @ApiProperty({ example: 'Bar Achdut', description: 'The name of the user' })
+  @IsString({ message: 'Name must be a string' })
+  @IsNotEmpty({ message: 'Name is required' })
   name: string;
 
   @ApiProperty({ example: 'bar.achdut@example.com', description: 'The email address of the user' })
+  @IsEmail({}, { message: 'Invalid email format' })
+  @IsNotEmpty({ message: 'Email is required' })
   email: string;
 
   @ApiProperty({ example: 'entrepreneur123', description: 'The password for the account' })
+  @IsString({ message: 'Password must be a string' })
+  @IsNotEmpty({ message: 'Password is required' })
+  @MinLength(8, { message: 'Password must be at least 8 characters long' })
   password: string;
 }
